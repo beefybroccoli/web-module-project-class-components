d0a330c1d37d1ab3e96b855d7adbf03549f77077
beefybroccoli <tedkimemail@yahoo.com>
2021-09-27 19:14:59 -0700

    initial

---
+++ .history/src/components/sample_data_20210927191256.js
@@ -0,0 +1,27 @@
+export const sample_data = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false,
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false,
+  },
+  {
+    task: "Get Rice",
+    id: 1528817084390,
+    completed: false,
+  },
+  {
+    task: "Get Chicken Soup",
+    id: 1528817084392,
+    completed: false,
+  },
+  {
+    task: "Get Fried Chic",
+    id: 1528817084394,
+    completed: false,
+  },
+];
